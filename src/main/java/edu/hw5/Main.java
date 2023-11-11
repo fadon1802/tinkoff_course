090f3f7d0badcb05184c92b5e617dcb69e479e1f
fadon1802 <pochta.antona1@gmail.com>
2023-11-12 01:43:00 +0500

    task4

--- src/main/java/edu/hw5/Main.java
+++ src/main/java/edu/hw5/Main.java
@@ -9,5 +9,7 @@ public final class Main {
         System.out.println(Task1.getAverageDuration("2022-03-12, 20:20 - 2022-03-12, 23:50\n"
             + "2022-04-01, 21:30 - 2022-04-02, 01:20"));
         System.out.println(Task2.getFridays13th(1925));
+        System.out.println(Task2.getNextFriday13("1925-05-21"));
+        System.out.println(Task4.hasSpecificSymbol("dfdd^fdyhyf"));
     }
 }
--- src/main/java/edu/hw5/Task2.java
+++ src/main/java/edu/hw5/Task2.java
@@ -3,6 +3,9 @@ package edu.hw5;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjuster;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +15,7 @@ public class Task2 {
     private Task2() {
     }
 
+    @SuppressWarnings("MagicNumber")
     public static List<String> getFridays13th(int year) {
         var fridays13th = new ArrayList<String>();
         var date = LocalDate.of(year, Month.JANUARY, 1);
@@ -28,5 +32,20 @@ public class Task2 {
         }
 
         return fridays13th;
+    }
+
+    @SuppressWarnings("MagicNumber")
+    public static String getNextFriday13(String date) {
+        var localDate = LocalDate.parse(date);
+        TemporalAdjuster adjuster = t -> {
+            while (t.get(ChronoField.DAY_OF_WEEK) != 5
+                || t.get(ChronoField.DAY_OF_MONTH) != 13) {
+                t = t.plus(1, ChronoUnit.DAYS);
+            }
+
+            return t;
+        };
+
+        return localDate.with(adjuster).toString();
     }
 }
